fd32d91e3986bb6d2d088811e45f4f989b71e267
1 <1>
2021-06-20 21:23:01 +0300

    Initial commit

---
+++ ConsoleApp7/CLI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    public static class CLI
+    {
+        public static void SetProject(House house)
+        {
+            int basement, walls, door, window, roof;
+            Console.WriteLine("Проект дома. Введите количество:");
+            Console.Write("Фундамент - ");
+            basement = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Стены - ");
+            walls = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Дверь - ");
+            door = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Окно - ");
+            window = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Крыша - ");
+            roof = Convert.ToInt32(Console.ReadLine());
+            house.SetHouse(basement, walls, door, window, roof);
+        }
+        
+        public static void PrintHouse(House house)
+        {
+            Console.WriteLine($"Проект дома:");
+            Console.WriteLine($"Фундамент - {house.GetBasement()} шт.");
+            Console.WriteLine($"Стены - {house.GetWalls()} шт.");
+            Console.WriteLine($"Дверь - {house.GetDoor()} шт.");
+            Console.WriteLine($"Окно - {house.GetWindow()} шт.");
+            Console.WriteLine($"Крыша - {house.GetRoof()} шт.");
+        }
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,17 @@
+﻿using static ConsoleApp7.CLI;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main()
+        {
+            var house = new House();
+            SetProject(house);
+            
+            PrintHouse(house);
+            
+            
+        }
+    }
+}
